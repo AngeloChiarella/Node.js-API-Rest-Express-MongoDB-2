6122ea3be59e75a4caea28b8feb6f5011a1f5794
AngeloChiarella <chiarella.gaetano@gmail.com>
2023-06-30 10:01:26 -0300

    Tratando pag 404

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import db from "./config/dbConnetc.js";
 import routes from "./routes/index.js";
 import manipuladorDeErros from "./middlewares/manipuladorDeErros.js";
+import manipulador404 from "./middlewares/manipulador404.js";
 
 db.on("error", console.log.bind(console, "Erro de conexao"));
 db.once("open", () => {
@@ -9,9 +10,10 @@ db.once("open", () => {
 });
 
 const app = new express();
-
 app.use(express.json()); // interpretar oo que chega e transformar em um obj
 routes(app);
+
+app.use(manipulador404);
 
 // eslint-disable-next-line no-unused-vars
 app.use(manipuladorDeErros);
